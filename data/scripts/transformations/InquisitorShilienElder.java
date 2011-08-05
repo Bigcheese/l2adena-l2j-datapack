f13ce583ce7947dc4bcefe61b5ae2ed3cc77fac6
MELERIX <MELERIX@5288da4f-0efc-0310-bd40-8d490a2659fa>
2011-08-05 20:40:58 +0000

    BETA/STABLE: Minor update on Inquisitor Stances by osiride, also fixing [8250] due should be commented not deleted.
    git-svn-id: http://svn.l2jdp.com/branches/unstable/L2J_DataPack_BETA@8255 5288da4f-0efc-0310-bd40-8d490a2659fa

--- data/scripts/transformations/InquisitorShilienElder.java
+++ data/scripts/transformations/InquisitorShilienElder.java
@@ -23,22 +23,22 @@ public class InquisitorShilienElder extends L2Transformation
 	
 	public void transformedSkills()
 	{
+		int lvl = 1;
 		if (getPlayer().getLevel() > 43)
-		{
-			// Divine Punishment
-			getPlayer().addSkill(SkillTable.getInstance().getInfo(1523, getPlayer().getLevel() - 43), false);
-			// Divine Flash
-			getPlayer().addSkill(SkillTable.getInstance().getInfo(1528, getPlayer().getLevel() - 43), false);
-			// Holy Weapon
-			getPlayer().addSkill(SkillTable.getInstance().getInfo(1043, 1), false);
-			// Surrender to the Holy
-			getPlayer().addSkill(SkillTable.getInstance().getInfo(1524, getPlayer().getLevel() - 43), false);
-			// Divine Curse
-			getPlayer().addSkill(SkillTable.getInstance().getInfo(1525, getPlayer().getLevel() - 43), false);
-			getPlayer().setTransformAllowedSkills(new int[]{838,1523,1528,1524,1525,1430,1303,1059,1043});
-		}
-		else
-			getPlayer().setTransformAllowedSkills(new int[]{838,1430,1303,1059});
+			lvl = (getPlayer().getLevel() - 43);
+		
+		// Divine Punishment
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(1523, lvl), false);
+		// Divine Flash
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(1528, lvl), false);
+		// Holy Weapon
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(1043, 1), false);
+		// Surrender to the Holy
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(1524, lvl), false);
+		// Divine Curse
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(1525, lvl), false);
+		// Set allowed skills
+		getPlayer().setTransformAllowedSkills(new int[]{838,1523,1528,1524,1525,1430,1303,1059,1043});
 		// Switch Stance
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(838, 1), false);
 	}
@@ -51,16 +51,20 @@ public class InquisitorShilienElder extends L2Transformation
 	
 	public void removeSkills()
 	{
+		int lvl = 1;
+		if (getPlayer().getLevel() > 43)
+			lvl = (getPlayer().getLevel() - 43);
+		
 		// Divine Punishment
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(1523, getPlayer().getLevel() - 43), false);
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(1523, lvl), false);
 		// Divine Flash
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(1528, getPlayer().getLevel() - 43), false);
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(1528, lvl), false);
 		// Holy Weapon
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(1043, 1), false, false);
 		// Surrender to the Holy
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(1524, getPlayer().getLevel() - 43), false);
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(1524, lvl), false);
 		// Divine Curse
-		getPlayer().removeSkill(SkillTable.getInstance().getInfo(1525, getPlayer().getLevel() - 43), false);
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(1525, lvl), false);
 		// Switch Stance
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(838, 1), false);
 		
