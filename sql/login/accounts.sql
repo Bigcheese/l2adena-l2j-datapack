46f789083884a45ca5e0b51f4d92d7071cdc7da9
Michael J. Spencer <bigcheesegs@gmail.com>
2011-08-05 14:30:15 -0400

    Change login.accounts.password to be CHAR(70) to support bcrypt.

--- sql/login/accounts.sql
+++ sql/login/accounts.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `accounts` (
   `login` VARCHAR(45) NOT NULL default '',
-  `password` VARCHAR(45) ,
+  `password` CHAR(70) ,
   `lastactive` bigint(13) unsigned NOT NULL DEFAULT '0',
   `accessLevel` TINYINT NOT NULL DEFAULT 0,
   `lastIP` CHAR(15) NULL DEFAULT NULL,
---
+++ sql/login/updates/20110805update.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `accounts` CHANGE COLUMN `password` `password` CHAR(70) NULL DEFAULT NULL  ;
